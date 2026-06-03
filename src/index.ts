9c325c1b07e1a22967774cafe7dbc1fe3e23a92c
Alex Zemlyakov <alex@ndr.su>
2026-06-03 17:52:33 +0100

    Bind production server to 0.0.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import { serve } from "bun";
 import index from "./index.html";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const server = serve({
+  hostname: isProduction ? "0.0.0.0" : undefined,
+
   routes: {
     // Serve index.html for all unmatched routes.
     "/*": index,
   },
 
-  development: process.env.NODE_ENV !== "production" && {
+  development: !isProduction && {
     // Enable browser hot reloading in development
     hmr: true,
 
